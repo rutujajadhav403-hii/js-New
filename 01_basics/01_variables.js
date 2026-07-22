16c5d9b6c3e214396d8f4c000b68d09e4b4b50c0
rutujajadhav403-hii <rutujajadhav403@gmail.com>
2026-07-22 11:30:26 +0000

    variable learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// declarattion of constant and variables
+const accountID = 144553 // Const = Can't change
+let accountEmail = "Rutuja@googleplaced"
+var accountPassword = "1234"
+accountCity = "Mumbai" //without using variable we can store but not good method
+let accountState; //undefined
+
+//accountID = 2 // not allowed
+
+console.log(accountID);
+accountEmail = "1bd@gamil"
+accountPassword = "6789"
+accountCity = "Pune"
+/*
+Prefer not use var
+becuase of issue in block scope and functional scope
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity]); // making table 
