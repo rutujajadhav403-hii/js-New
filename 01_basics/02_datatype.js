c1f44b986c90212154c721b1f0c9bf40894c2cda
rutujajadhav403-hii <rutujajadhav403@gmail.com>
2026-07-22 12:33:34 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer JS
+
+// alert(3+3) //it will give error  ,we are using nodejs,not browser
+
+console.log("Rutuja");
+
+let name = "Sujata"
+let age = 45
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bignit
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+//symbol => uniqe
+
+//object
+console.log(typeof "Sujata"); //show type
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
